8a341c2e70a630972e9ba6e692a18c069e31adc5
awesomeness1jake <69776285+awesomeness1jake@users.noreply.github.com>
2022-10-15 14:59:53 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,10 @@ games { 'gta5' }
 
 lua54 'yes'
 
-client_script 'musicplayer.lua'
-client_script 'thread_misc.lua'
-client_script 'sys_lib.lua'
-client_script 'native_lib.lua'
-client_script 'client.lua'
-server_script 'server.lua'
+client_scripts {
+  'thread_misc.lua',
+  'native_lib.lua',
+  'sys_lib.lua',
+  'musicplayer.lua',
+  'client.lua',
+}
